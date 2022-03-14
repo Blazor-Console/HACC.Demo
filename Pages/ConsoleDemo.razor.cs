d38e5dfc6ea7432a37b360498ca2b97441408746
Jessica Mulein <jessica@brightchain.org>
2022-03-14 15:55:44 -0700

    rework structure

--- Pages/ConsoleDemo.razor.cs
+++ Pages/ConsoleDemo.razor.cs
@@ -1,26 +1,27 @@
 using System.Runtime.Versioning;
 using HACC.Applications;
+using HACC.Components;
 using HACC.Enumerations;
+using HACC.Models.Drivers;
 using Microsoft.AspNetCore.Components;
 
 namespace HACC.Demo.Pages;
 [SupportedOSPlatform(platformName: "browser")]
 public partial class ConsoleDemo : ComponentBase
 {
-    private ConsoleType ActiveConsole { get; } = ConsoleType.StandardOutput;
 
     [Inject] private ILogger Logger { get; set; } = null!;
 
     public readonly WebApplication WebApplication;
 
     // ReSharper disable once MemberInitializerValueIgnored
-    public HACC.Components.Console ConsoleReference { get; set; } = null!;
+    public WebConsole ConsoleReference { get; set; } = null!;
 
     public ConsoleDemo()
     {
         this.WebApplication = new WebApplication(
             logger: this.Logger,
-            console: this.ConsoleReference!);
+            console: this.ConsoleReference);
     }
 
     protected override void OnAfterRender(bool firstRender)
