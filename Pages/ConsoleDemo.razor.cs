3ff842f1b8e7ecee9dcd968b1a92b8ef0152a614
Jessica Mulein <jessica@mulein.com>
2022-03-14 13:38:22 -0700

    fix index/console page

---
+++ Pages/ConsoleDemo.razor.cs
@@ -0,0 +1,26 @@
+using System.Runtime.Versioning;
+using HACC.Applications;
+using HACC.Enumerations;
+using Microsoft.AspNetCore.Components;
+
+namespace HACC.Demo.Pages;
+[SupportedOSPlatform(platformName: "browser")]
+public partial class ConsoleDemo : ComponentBase
+{
+    private ConsoleType ActiveConsole { get; } = ConsoleType.StandardOutput;
+
+    [Inject] private ILogger Logger { get; set; } = null!;
+
+    public readonly WebApplication WebApplication;
+
+    // ReSharper disable once MemberInitializerValueIgnored
+    public HACC.Components.Console ConsoleReference { get; set; } = null!;
+
+    public ConsoleDemo()
+    {
+        this.WebApplication = new WebApplication(
+            logger: this.Logger,
+            console: this.ConsoleReference!);
+        this.WebApplication.Init();
+    }
+}
--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,28 +1,7 @@
-using System.Runtime.Versioning;
-using HACC.Applications;
-using HACC.Enumerations;
-using Microsoft.AspNetCore.Components;
-using Console = HACC.Components.Console;
 
 namespace HACC.Demo.Pages;
 
-[SupportedOSPlatform(platformName: "browser")]
 public partial class Index
 {
-    private ConsoleType ActiveConsole { get; } = ConsoleType.StandardOutput;
-
-    [Inject] private ILogger Logger { get; set; } = null!;
-
-    public readonly WebApplication WebApplication;
-
-    // ReSharper disable once MemberInitializerValueIgnored
-    public Console ConsoleReference { get; set; } = null!;
-
-    public Index()
-    {
-        this.WebApplication = new WebApplication(
-            logger: this.Logger,
-            console: this.ConsoleReference!);
-        this.WebApplication.Init();
-    }
+   
 }
