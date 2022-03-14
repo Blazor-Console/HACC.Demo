01debadc9aef43f2f7505a110f922d64d39a9f44
Jessica Mulein <jessica@mulein.com>
2022-03-14 12:57:39 -0700

    new application setup

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,8 +1,14 @@
 using HACC.Enumerations;
+using Microsoft.AspNetCore.Components;
+using Console = HACC.Components.Console;
 
 namespace HACC.Demo.Pages;
 
 public partial class Index
 {
-    private ConsoleType ActiveConsole { get; set; }
+    private ConsoleType ActiveConsole { get; }
+
+    [Inject] private ILogger Logger { get; set; } = null!;
+
+    public Console ConsoleReference { get; set; } = null!;
 }
