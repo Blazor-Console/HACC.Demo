a54e256fac2dfcbbbc7673d806ac1ee7f14e20dc
Jessica Mulein <jessica@mulein.com>
2022-03-14 13:01:56 -0700

    make member var

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,14 +1,27 @@
+using System.Runtime.Versioning;
+using HACC.Applications;
 using HACC.Enumerations;
 using Microsoft.AspNetCore.Components;
 using Console = HACC.Components.Console;
 
 namespace HACC.Demo.Pages;
 
+[SupportedOSPlatform(platformName: "browser")]
 public partial class Index
 {
     private ConsoleType ActiveConsole { get; }
 
     [Inject] private ILogger Logger { get; set; } = null!;
 
+    public readonly WebApplication WebApplication;
+
     public Console ConsoleReference { get; set; } = null!;
+    
+    public Index()
+    {
+        ActiveConsole = ConsoleType.StandardOutput;
+        this.WebApplication = new WebApplication(
+            logger: Logger,
+            console: ConsoleReference);
+    }
 }
