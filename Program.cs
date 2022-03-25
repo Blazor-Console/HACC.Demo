24db3cd3dfc260ea7aa6d174f78462e6e670c345
Jessica Mulein <jessica@brightchain.org>
2022-03-25 09:30:29 -0700

    simplify startup

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-using HACC.Demo;
+using HACC.Demo.Extensions;
 using HACC.Extensions;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args: args);
-builder.RootComponents.Add<App>(selector: "#app");
-builder.RootComponents.Add<HeadOutlet>(selector: "head::after");
-builder.Services.AddScoped(implementationFactory: sp => new HttpClient
-    {BaseAddress = new Uri(uriString: builder.HostEnvironment.BaseAddress)});
-builder.UsingHacc();
-await builder.Build().RunAsync();
+await WebAssemblyHostBuilder
+    .CreateDefault(args: args)
+    .UseBlazor()
+    .UseHacc()
+    .Build()
+    .RunAsync();
