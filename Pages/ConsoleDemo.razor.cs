28f99e88a1d01cef28faece08eb9e275c43aaac2
Jessica Mulein <jessica@brightchain.org>
2022-03-26 09:39:17 -0700

    fix webConsole context reference

--- Pages/ConsoleDemo.razor.cs
+++ Pages/ConsoleDemo.razor.cs
@@ -6,11 +6,17 @@ namespace HACC.Demo.Pages;
 
 public partial class ConsoleDemo : ComponentBase
 {
+    /// <summary>
+    /// This is NULL until after after render
+    /// </summary>
     private WebConsole _webConsole = default!;
 
     protected override void OnAfterRender(bool firstRender)
     {
         base.OnAfterRender(firstRender);
+        if (_webConsole is null)
+            throw new InvalidOperationException("_webConsole reference was not set");
+
         this._webConsole.WebApplication.Shutdown();
         this._webConsole.WebApplication.Init();
         Application.Top.Add(view: new Label(text: "HACC Demo"));
